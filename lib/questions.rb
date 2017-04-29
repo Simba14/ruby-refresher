736a0681cdc054e8f44312d5a10bfef5dcfbb8ee
Simba14 <seanblundell@gmail.com>
2017-04-29 23:45:35 +0100

    Complete 34: is a 3 dot range

--- lib/questions.rb
+++ lib/questions.rb
@@ -212,6 +212,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.to_a != (range.first...(range.last+1)).to_a
 end
 
 # get the square root of a number
